523fae81fd2c1aea1670521fb89ff15209e2cc39
cl3m3nt0n1 <clement.meslif@free.fr>
2024-10-18 12:07:04 +0200

    [ADD] Added a reverb component to handle everything related to the reverb apvts' parameters.

---
+++ source/ReverbComponent/ReverbComponent.cpp
@@ -0,0 +1,80 @@
+#include "ReverbComponent.hpp"
+#include "juce_events/juce_events.h"
+#include "juce_graphics/juce_graphics.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+
+ReverbComponent::ReverbComponent(juce::AudioProcessorValueTreeState& audioTree) :
+    mbackgroundColour(juce::Colour::fromRGB(225, 90, 151)),
+    apvts(audioTree),
+    mReverbDampingSliderAttachement(apvts, "Reverb Damping", mReverbDampingSlider.getslider()),
+    mReverbRoomSizeSliderAttachement(apvts, "Reverb Room Size",mReverbRoomSizeSlider.getslider()),
+    mReverbWetSliderAttachement(apvts, "Reverb Wet", mReverbWetSlider.getslider()), 
+    mReverbDrySliderAttachement(apvts, "Reverb Dry", mReverbDrySlider.getslider()),
+    mReverbWidthSliderAttachement(apvts, "Reverb Width",mReverbWidthSlider.getslider()),
+    mReverbFreezeToggleAttachement(apvts, "Reverb Freeze", mReverbFreezeToggle.getToggle())
+{
+
+    addAndMakeVisible(mReverbFreezeToggle);
+    addAndMakeVisible(mReverbDampingSlider );
+    addAndMakeVisible(mReverbRoomSizeSlider);
+    addAndMakeVisible(mReverbWetSlider );
+    addAndMakeVisible(mReverbDrySlider);
+    addAndMakeVisible(mReverbWidthSlider);
+}
+
+ReverbComponent::~ReverbComponent()
+{
+}
+
+void ReverbComponent::paint (juce::Graphics& g)
+{
+    g.fillAll(mbackgroundColour);
+}
+
+void ReverbComponent::resized()
+{
+    juce::Grid grid;
+    using Track = juce::Grid::TrackInfo;
+    using Fr = juce::Grid::Fr;
+
+
+    grid.templateRows = { 
+        Track (Fr (1)), 
+        Track (Fr (4)), 
+        Track (Fr (4)), 
+        Track (Fr (4)), 
+        Track (Fr (1))
+        };
+        
+    grid.templateColumns = { 
+        Track (Fr (1)), 
+        Track (Fr (3)), 
+        Track (Fr (1)), 
+        Track (Fr (3)), 
+        Track (Fr (1))
+    };
+
+    for (int i = 0; i < 25; ++i)
+        grid.items.set(i, juce::GridItem(nullptr));
+
+    
+    grid.items.set(6,  mReverbDampingSlider);
+    grid.items.set(8,  mReverbFreezeToggle);
+    grid.items.set(11, mReverbDrySlider);
+    grid.items.set(13, mReverbWetSlider);
+    grid.items.set(16, mReverbRoomSizeSlider);
+    grid.items.set(18, mReverbWidthSlider);
+
+    grid.performLayout (getLocalBounds());
+}
+
+std::vector<juce::Component*> ReverbComponent::getSliders()
+{
+    return {
+        &mReverbDampingSlider,
+        &mReverbRoomSizeSlider,
+        &mReverbWetSlider,
+        &mReverbDrySlider,
+        &mReverbWidthSlider
+    };
+}
---
+++ source/ReverbComponent/ReverbComponent.hpp
@@ -0,0 +1,168 @@
+#include "../CustomLookNFeel/CustomLookNFeel.h"
+#include "juce_audio_processors/juce_audio_processors.h"
+#include "juce_core/juce_core.h"
+#include "juce_core/system/juce_PlatformDefs.h"
+#include "juce_events/juce_events.h"
+#include "juce_graphics/juce_graphics.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+#include <vector>
+
+/**
+ * @brief a basic struct inheriting from juce::component
+ *        to hold a slider and a label arranged in a 
+ *        simple grid way.
+ * 
+ */
+struct SliderAndLabel : public juce::Component
+{
+public:
+    SliderAndLabel(juce::String sliderLabel) :slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                      juce::Slider::TextEntryBoxPosition::TextBoxAbove)
+    {
+        juce::FontOptions fontOptions("JetBrainsMono Nerd Font", "Bold", 18.0f);
+        
+        label.setFont(fontOptions);
+        label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::black);
+        label.setJustificationType(juce::Justification::centredTop);
+        label.setText(sliderLabel, juce::NotificationType::dontSendNotification);
+
+        addAndMakeVisible(slider);
+        addAndMakeVisible(label);
+    }
+
+    void paint(juce::Graphics& g) override
+    {
+
+    }
+
+    void resized() override
+    {
+        juce::Grid grid;
+        using Track = juce::Grid::TrackInfo;
+        using Fr = juce::Grid::Fr;
+
+        grid.templateRows = 
+        {
+            Track(Fr(2)),
+            Track(Fr(1))
+        };
+
+        grid.templateColumns = 
+        {
+            Track(Fr(1))
+        };
+
+        grid.items.set(0, slider);
+        grid.items.set(1, label);
+
+        grid.performLayout(getLocalBounds());
+    }
+
+    juce::Slider& getslider() { return slider; }
+    
+    
+private:
+    juce::Slider slider;
+    juce::Label label;
+};
+
+
+/**
+ * @brief Same thing but with a juce::ToggleButton.
+ */
+struct ToggleAndLabel : public juce::Component
+{
+public:
+    ToggleAndLabel(juce::String sliderLabel)
+    {
+        juce::FontOptions fontOptions("JetBrainsMono Nerd Font", "Bold", 18.0f);
+        
+        label.setFont(fontOptions);
+        label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::black);
+        label.setJustificationType(juce::Justification::centredTop);
+        label.setText(sliderLabel, juce::NotificationType::dontSendNotification);
+    
+        addAndMakeVisible(toggle);
+        addAndMakeVisible(label);
+
+    }
+
+    void paint(juce::Graphics& g) override
+    {
+
+    }
+
+    void resized() override
+    {
+        juce::Grid grid;
+        using Track = juce::Grid::TrackInfo;
+        using Fr = juce::Grid::Fr;
+
+        grid.templateRows = 
+        {
+            Track(Fr(2)),
+            Track(Fr(1))
+        };
+
+        grid.templateColumns = 
+        {
+            Track(Fr(1))
+        };
+
+        grid.items.set(0, toggle);
+        grid.items.set(1, label);
+
+        grid.performLayout(getLocalBounds());
+    }
+
+    juce::ToggleButton& getToggle() { return toggle; }
+    
+    
+private:
+    juce::ToggleButton toggle;
+    juce::Label label;
+};
+
+/**
+ * @brief The juce::component related to the reverb
+ *        instance of the plugin.
+ */
+class ReverbComponent : public juce::Component
+{
+public:
+    ReverbComponent (juce::AudioProcessorValueTreeState& audioTree);
+
+    ~ReverbComponent() override;
+
+    void paint (juce::Graphics& g) override;
+
+    void resized() override;
+
+    std::vector<juce::Component*> getSliders();
+
+private:
+    juce::Colour mbackgroundColour;
+
+    juce::AudioProcessorValueTreeState& apvts;
+
+    // Reverb sliders and everything related
+    SliderAndLabel mReverbDampingSlider  {"REV::DMP"}, 
+                   mReverbRoomSizeSlider {"REV::RSZ"},
+                   mReverbWetSlider      {"REV::WET"}, 
+                   mReverbDrySlider      {"REV::DRY"},
+                   mReverbWidthSlider    {"REV::WID"};
+
+    juce::AudioProcessorValueTreeState::SliderAttachment
+                mReverbDampingSliderAttachement,
+                mReverbRoomSizeSliderAttachement,
+                mReverbWetSliderAttachement, 
+                mReverbDrySliderAttachement,
+                mReverbWidthSliderAttachement;
+
+    // Reverb Freeze Toggle and everything related
+    ToggleAndLabel mReverbFreezeToggle {"REV::FRZ"};
+    juce::AudioProcessorValueTreeState::ButtonAttachment
+                mReverbFreezeToggleAttachement;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReverbComponent)
+};
