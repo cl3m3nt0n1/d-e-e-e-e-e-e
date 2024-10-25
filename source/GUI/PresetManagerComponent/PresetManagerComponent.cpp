3b07719f9c44fbdfa29b1c43d87e2aa4cbad0186
cl3m3nt0n1 <clement.meslif@free.fr>
2024-10-25 12:46:26 +0200

    [UPD] Modified the format of xml presets to use attributes and therefore juce::xmlElement.

--- source/GUI/PresetManagerComponent/PresetManagerComponent.cpp
+++ source/GUI/PresetManagerComponent/PresetManagerComponent.cpp
@@ -1,7 +1,98 @@
 #include "PresetManagerComponent.hpp"
+#include "juce_core/system/juce_PlatformDefs.h"
+#include <memory>
+
 
 PresetManagerComponent::PresetManagerComponent()
 {
+    mPreviousButton.setButtonText("Previous");
+    mNextButton.setButtonText("Next");
+    mSaveButton.setButtonText("Save");
+    mDeleteButton.setButtonText("Delete");
+
+    mComboBox.setTextWhenNothingSelected("Select preset");
+
+    auto preset = loadPresetFromXML(juce::File("/home/clementone/Documents/DEVS/d-e-e-e-e-e-e/Resources/preset_test_1.xml"));
+    preset.print();
 
 
+    for( auto button : getButtons() )
+    {
+        button->setMouseCursor(juce::MouseCursor::PointingHandCursor);
+        addAndMakeVisible(button);
+    }
+
+
+}
+
+Preset PresetManagerComponent::loadPresetFromXML(juce::File xmlFile)
+{
+    if(xmlFile.existsAsFile())
+    {
+        Preset preset;
+        // parsing the xml file 
+        auto element = juce::XmlDocument::parse(xmlFile); // same as creating an object and calling getDocumentElement()
+        if(element->hasTagName("PRESET"))
+        {
+            /*
+            * There, the goal is to fetch every data from every node.
+            * Our nodes are:
+            *  - INFORMATION
+            *  - PARAMETERS
+            *      - DELAY 
+            *      - REVERB 
+            *      - PLUGIN 
+            */
+            for(auto* e : element->getChildIterator())
+            {
+                if (e->hasTagName("INFORMATION"))
+                {
+                    preset.presetName = e->getStringAttribute("Name");
+                    preset.authorName = e->getStringAttribute("Author");
+                    preset.category   = e->getStringAttribute("Category");
+                }
+                else if (e->hasTagName("PARAMETERS")) 
+                {
+                    for(auto* child : e->getChildIterator())
+                    {
+                        if(child->hasTagName("DELAY"))
+                        {
+                            preset.delayTime            = child->getIntAttribute("Time");
+                            preset.delayFeedback        = (float)child->getDoubleAttribute("Feedback");
+                            preset.delaySyncToggleState = child->getBoolAttribute("SyncToggle");
+                            preset.delaySyncDivider     = child->getIntAttribute("SyncDivider");
+                        }
+                        else if (child->hasTagName("REVERB")) 
+                        {
+                            preset.reverbDamping        = (float)child->getDoubleAttribute("Damping");
+                            preset.reverbFreezeState    = child->getBoolAttribute("Freeze");
+                            preset.reverbRoomSize       = (float)child->getDoubleAttribute("RoomSize");
+                            preset.reverbWet            = (float)child->getDoubleAttribute("Wet");
+                            preset.reverbDry            = (float)child->getDoubleAttribute("Dry");
+                            preset.reverbWidth          = (float)child->getDoubleAttribute("Width");
+                        }
+                        else if (child->hasTagName("PLUGIN"))
+                        {
+                            preset.pluginDryWet         = (float)child->getDoubleAttribute("DryWet");
+                            preset.pluginLevel          = (float)child->getDoubleAttribute("Level");
+                            preset.pluginGain           = (float)child->getDoubleAttribute("Gain");
+                        }
+                    }
+                }
+            }
+        }
+        return preset;
+    }
+    return {};
+}
+
+std::vector<juce::TextButton*> PresetManagerComponent::getButtons()
+{
+    return 
+    {
+        &mPreviousButton,
+        &mNextButton,
+        &mDeleteButton,
+        &mSaveButton
+    };
 }
--- source/GUI/PresetManagerComponent/PresetManagerComponent.hpp
+++ source/GUI/PresetManagerComponent/PresetManagerComponent.hpp
@@ -1,19 +1,69 @@
 #pragma once
 
+#include "juce_core/juce_core.h"
+#include "juce_core/system/juce_PlatformDefs.h"
 #include "juce_gui_basics/juce_gui_basics.h"
 #include <juce_audio_basics/juce_audio_basics.h>
 
-class PresetManagerComponent 
+/**
+ * @brief a simple struct to contain every parameter and 
+ *        values of a preset dumped from an XML file. 
+ */
+struct Preset 
+{
+    // PRESET INFO
+    juce::String presetName    = {};
+    juce::String authorName    = {};
+    juce::String category      = {};
+
+    // PARAMETERS VALUES
+    //    DELAY 
+    int   delayTime            = 0;
+    float delayFeedback        = 0.0f;
+    bool  delaySyncToggleState = false;
+    int   delaySyncDivider     = 1;
+
+    //    REVERB
+    float reverbDamping        = 0.0f;
+    bool  reverbFreezeState    = false;
+    float reverbRoomSize       = 0.0f;
+    float reverbWet            = 0.0f;
+    float reverbDry            = 0.0f;
+    float reverbWidth          = 0.0f;
+
+    //    PLUGIN
+    float pluginDryWet         = 0.0f;
+    float pluginLevel          = 0.0f;
+    float pluginGain           = 0.0f;
+
+    /* ========= MEMBERS ========== */
+    void print()
+    {
+        // DBG PURPOSES ONLY
+        DBG("presetName:");        DBG(presetName);
+        DBG("authorName:");        DBG(authorName);
+        DBG("category:");          DBG(category);
+        DBG("delayTime:");         DBG(delayTime);
+        DBG("reverbDamping:");     DBG(reverbDamping);
+        DBG("pluginDryWet:");      DBG(pluginDryWet);
+    }
+};
+
+class PresetManagerComponent : public juce::Component
 {
 public:
 
     PresetManagerComponent();
 
-private:
+    Preset loadPresetFromXML(juce::File xmlFile);
 
-    juce::TableListBox mListBox;
-    
-    
+private:
+    /* =================== MEMBERS ======================= */
+    juce::TextButton mPreviousButton, mNextButton, mSaveButton, mDeleteButton;
+    juce::ComboBox mComboBox; // Right now, only a combo box. TODO: Replace with PrestListBoxComponent
+
+    /* =================== METHODS ======================= */
+    std::vector<juce::TextButton*> getButtons();
 
 JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PresetManagerComponent)
 };
